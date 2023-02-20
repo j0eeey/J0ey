0da38604765fdc441cd9b43a4ddbfd75fe207677
j0eeey <2998590202@qq.com>
2023-02-20 20:40:28 +0800

    Changes

---
+++ src/components/MyList.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="todo-main">
+    <MyItem/>
+  </ul>
+</template>
+
+<script>
+import MyItem from "@/components/MyItem";
+export default {
+  name: "MyList",
+  components: {MyItem}
+}
+</script>
+
+<style scoped>
+
+</style>
